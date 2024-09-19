9afedd1cd933a40d5bd7cc50b64de4add07b5e6b
Michael <kinovar13@gmail.com>
2024-09-20 04:07:15 +0700

    first commit

---
+++ src/SixthAndSeventhTask.java
@@ -0,0 +1,16 @@
+public class SixthTask {
+    int a;
+    public void main(){
+        if( a < 0 ){
+            System.out.println("отрицательное");
+        } else if (a == 0) {
+            System.out.println("0");
+
+        }
+        else{
+            System.out.println("число положительное");
+        }
+
+
+    }
+}
